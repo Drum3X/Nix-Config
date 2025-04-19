f4e259575990f799931d78c4f5254d071d1d3f38
Drum3X <drum3x@tutamail.com>
2025-04-19 14:20:12 +0300

    fuuuuuuuuck tayyip

--- machines/nitro/home/programs/terminal/shell/nushell.nix
+++ machines/nitro/home/programs/terminal/shell/nushell.nix
@@ -8,5 +8,9 @@
       resystem = "sudo nixos-rebuild switch --flake .#nitro";
       rehome = "home-manager switch --flake .#drum3x@nitro";
     };
+
+    extraConfig = ''
+      $env.config.show_banner = false
+    '';
   };
 }
