69ccbfcf05b67e0cb03c4f43e98ee0f94be22bdb
Drum3X <drum3x@tutamail.com>
2025-04-28 11:14:21 +0300

    huge commit

--- machines/nitro/core/modules/hardware.nix
+++ machines/nitro/core/modules/hardware.nix
@@ -9,28 +9,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/b93f74df-2652-4221-bfc6-a626c92db8e4";
-      fsType = "ext4";
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/E718-E783";
-      fsType = "vfat";
-      options = [
-        "fmask=0022"
-        "dmask=0022"
-      ];
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-uuid/f78bc266-84aa-4efa-a931-9677ddf3d601";
-    }
-  ];
-
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
