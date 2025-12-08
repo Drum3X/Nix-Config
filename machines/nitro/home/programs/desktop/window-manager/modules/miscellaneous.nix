5a9af91ca36a6bd475fcfc246218b74611c7a50c
Drum3X <drum3x@tutamail.com>
2025-12-08 15:10:26 +0300

    Please provide the file changes (diffs) so I can generate a commit message.

--- machines/nitro/home/programs/desktop/window-manager/modules/miscellaneous.nix
+++ machines/nitro/home/programs/desktop/window-manager/modules/miscellaneous.nix
@@ -1,6 +1,16 @@
 {config, ...}: {
   programs.niri.settings = {
-    environment = config.home.sessionVariables;
+    environment = {
+      QT_QPA_PLATFORM = "wayland";
+    };
+
+    cursor = {
+      theme = config.home.pointerCursor.name;
+      size = config.home.pointerCursor.size;
+
+      hide-when-typing = true;
+      hide-after-inactive-ms = 3000;
+    };
 
     overview = {
       zoom = 0.8;
